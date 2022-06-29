3e02d3526abb1338b03faf5c0d70dc4af6ff941c
Giovanni Ambrosio <g.ambro@gmail.com>
2022-06-29 13:26:34 +0200

    7 - edited styles

--- client/src/list/list-item.jsx
+++ client/src/list/list-item.jsx
@@ -3,7 +3,8 @@ import TableCell from '@mui/material/TableCell';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import { itemStatus } from '../constants';
 
 const styles = {
 	general: {
@@ -20,7 +21,12 @@ const styles = {
 		background: 'rgba(65, 203, 113, 0.1)',
 	},
 	deleted: {
-		background: 'red',
+		border: '1px dashed #AAA',
+		background: 'rgba(0,0,0, 0.1)',
+		color: "#FFF",
+		cursor: 'not-allowed',
+		pointerEvents: 'none',
+		textShadow: '0px 1px 1px #BBB'
 	},
 	created: {
 		border: '1px solid #FFA900',
@@ -33,14 +39,18 @@ const styles = {
 	}
 }
 
-
 const ListItem = ({ item, onEdit, onDelete }) => {
 	return (
 		<MUITableRow style={{ ...styles.general, ...styles[item.status] }}>
-			<TableCell sx={{ width: 50 }} align="center">
+			<TableCell sx={{ width: 100 }} align="center">
 				{item.status && `[${item.status}]`}
 			</TableCell>
-			<TableCell>{item.title}</TableCell>
+			<TableCell style={{ flexGrow: '1' }}>
+				{([itemStatus.PROCESSING, itemStatus.CREATED].includes(item.status)) && <IconButton color="primary" onClick={() => onEdit(item)}>
+					<AccessTimeFilledIcon />
+				</IconButton>}
+				{item.title}
+			</TableCell>
 			<TableCell sx={{ width: 170 }} align='right'>
 				<IconButton color="primary" onClick={() => onEdit(item)}>
 					<EditIcon />
